643bf9ad0f4406f74ba496e3a14ba0cf6397c926
Ruslan <medtehnik850@gmail.com>
2022-04-14 14:09:20 +0700

    Initial commit

---
+++ src/main/java/spring_data_jpa/entity/Employee.java
@@ -0,0 +1,52 @@
+package spring_data_jpa.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "salary")
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
